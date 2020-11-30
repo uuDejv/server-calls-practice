3931405a7c45160a108d04dc9a56683715b40853
David Vlasak <davidvlasak2097@gmail.com>
2020-11-30 13:57:58 +0100

    Hotfixing async await

--- 1 - simple-react-table/react-frontend/index.js
+++ 1 - simple-react-table/react-frontend/index.js
@@ -2,5 +2,6 @@ import styled from "@emotion/styled";
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./layout";
+import "regenerator-runtime/runtime";
 
 ReactDOM.render(<Layout />, document.getElementById("♥"));
